173d83621ac3a1482188358b161e2b114df6fa8f
Abhi <abhirup@shyft.to>
2023-06-28 02:31:27 +0530

    Added candy minting and montitors

--- src/pages/api/candymachine-update-owners.ts
+++ src/pages/api/candymachine-update-owners.ts
@@ -33,7 +33,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
         else
             throw new Error("WRONG_NETWORK");
         
-        var cm_mints:string[] = [];
+        var cm_mints:any[] = [];
 
         const getMintsFromCandyMachine = await shyftClient.candyMachine.readNfts({
             network: shyftNetwork,
@@ -44,21 +44,59 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
         if(getMintsFromCandyMachine.nfts?.length > 0){
             for (let index = 0; index < getMintsFromCandyMachine.nfts.length; index++) {
                 const nft = getMintsFromCandyMachine.nfts[index];
+                cm_mints.push(nft);
+                
+                var objectToBePushed = {};
+
+                const mintExists = await supabase
+                    .from('monitor_mints')
+                    .select()
+                    .eq("mint_address",nft.mint);
+                
+                if(mintExists.count !== null)
+                    objectToBePushed = mintExists.data[0];
+
+                objectToBePushed ={ ...objectToBePushed, mint_address: nft.mint, current_holder: nft.owner};
+
                  //get from DB then push
-                const insertToDb = await supabase.from('monitor_mints').upsert({
-                    mint_address: nft.mint,
-                    current_holder: nft.owner
-                });
+                const insertToDb = await supabase.from('monitor_mints').upsert(objectToBePushed);
                 if (insertToDb.error !== null)
                     throw new Error('INSERT_TO_DB_FAILED');
-                
             }
         }
         else
             throw new Error("NO_NFTS_IN_CM");
         
+        res.status(200).json({
+            success: true,
+            message: "All Nfts in this CM",
+            result: cm_mints
+        }); 
 
-    } catch (error) {
-        
+    } catch (error:any) {
+        if(error.message === "WRONG_NETWORK")
+            res.status(400).json({
+                success: false,
+                message: "Invalid Network",
+                result: []
+            });
+        else if(error.message === "INSERT_TO_DB_FAILED")
+            res.status(400).json({
+                success: false,
+                message: "Could not insert to DB",
+                result: []
+            });
+        else if(error.message === "NO_NFTS_IN_CM")
+            res.status(404).json({
+                success: false,
+                message: "No Nfts found in CM",
+                result: []
+            });
+        else
+            res.status(500).json({
+                success: false,
+                message: "Internal Server Error",
+                result: []
+            }); 
     }
 }
